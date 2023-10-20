47077db1fc2835989b1c7295808e256a9b0d793c
Youngcrab <123295849+cantthinkofanickname@users.noreply.github.com>
2023-10-20 15:34:23 +0300

    Update login.py

--- steampy/login.py
+++ steampy/login.py
@@ -5,6 +5,8 @@ from steampy import guard
 import rsa
 from steampy.models import SteamUrl
 from steampy.exceptions import InvalidCredentials, CaptchaRequired
+import json
+
 
 
 class LoginExecutor:
@@ -15,13 +17,21 @@ class LoginExecutor:
         self.one_time_code = ''
         self.shared_secret = shared_secret
         self.session = session
+        self.client_id = ''
+        self.steamid = ''
+        self.request_id = ''
+        self.refresh_toker = ''
+        self.nonce_store = ''
+        self.auth_store = ''
+        self.nonce_com = ''
+        self.auth_com = ''
 
     def login(self) -> requests.Session:
         login_response = self._send_login_request()
-        self._check_for_captcha(login_response)
-        login_response = self._enter_steam_guard_if_necessary(login_response)
-        self._assert_valid_credentials(login_response)
-        self._perform_redirects(login_response.json())
+        self._update_stem_guard(login_response)
+        self._pool_sessions_steam()
+        finallized_response = self._finallez_login()
+        self._setstokens(finallized_response)
         self.set_sessionid_cookies()
         return self.session
 
@@ -30,7 +40,9 @@ class LoginExecutor:
         encrypted_password = self._encrypt_password(rsa_params)
         rsa_timestamp = rsa_params['rsa_timestamp']
         request_data = self._prepare_login_request_data(encrypted_password, rsa_timestamp)
-        return self.session.post(SteamUrl.COMMUNITY_URL + '/login/dologin', data=request_data)
+        response = self.session.post(SteamUrl.BeginAuthSessionViaCredentials_URL, data=request_data)
+        return response
+
 
     def set_sessionid_cookies(self):
         sessionid = self.session.cookies.get_dict()['sessionid']
@@ -49,12 +61,13 @@ class LoginExecutor:
 
     def _fetch_rsa_params(self, current_number_of_repetitions: int = 0) -> dict:
         maximal_number_of_repetitions = 5
-        key_response = self.session.post(SteamUrl.COMMUNITY_URL + '/login/getrsakey/',
-                                         data={'username': self.username}).json()
+        self.session.post(SteamUrl.COMMUNITY_URL)
+        response = self.session.get(SteamUrl.GetPasswordRSAPublicKey_URL + self.username)
+        key_response = json.loads(response.text)
         try:
-            rsa_mod = int(key_response['publickey_mod'], 16)
-            rsa_exp = int(key_response['publickey_exp'], 16)
-            rsa_timestamp = key_response['timestamp']
+            rsa_mod = int(key_response["response"]['publickey_mod'], 16)
+            rsa_exp = int(key_response["response"]['publickey_exp'], 16)
+            rsa_timestamp = key_response["response"]['timestamp']
             return {'rsa_key': rsa.PublicKey(rsa_mod, rsa_exp),
                     'rsa_timestamp': rsa_timestamp}
         except KeyError:
@@ -68,17 +81,10 @@ class LoginExecutor:
 
     def _prepare_login_request_data(self, encrypted_password: str, rsa_timestamp: str) -> dict:
         return {
-            'password': encrypted_password,
-            'username': self.username,
-            'twofactorcode': self.one_time_code,
-            'emailauth': '',
-            'loginfriendlyname': '',
-            'captchagid': '-1',
-            'captcha_text': '',
-            'emailsteamid': '',
-            'rsatimestamp': rsa_timestamp,
-            'remember_login': 'true',
-            'donotcache': str(int(time.time() * 1000))
+            'persistence': "1",
+            'encrypted_password': encrypted_password,
+            'account_name': self.username,
+            'encryption_timestamp': rsa_timestamp,
         }
 
     @staticmethod
@@ -94,8 +100,8 @@ class LoginExecutor:
 
     @staticmethod
     def _assert_valid_credentials(login_response: requests.Response) -> None:
-        if not login_response.json()['success']:
-            raise InvalidCredentials(login_response.json()['message'])
+        if not login_response.json()['response']["client_id"]:
+            raise InvalidCredentials(login_response.json()["response"]['extended_error_message'])
 
     def _perform_redirects(self, response_dict: dict) -> None:
         parameters = response_dict.get('transfer_parameters')
@@ -106,3 +112,70 @@ class LoginExecutor:
 
     def _fetch_home_page(self, session: requests.Session) -> requests.Response:
         return session.post(SteamUrl.COMMUNITY_URL + '/my/home/')
+
+
+    def _update_stem_guard(self, login_response):
+        response_json = json.loads(login_response.text)
+        self.client_id = response_json["response"]["client_id"]
+        self.steamid = response_json["response"]["steamid"]
+        self.request_id = response_json["response"]["request_id"]
+        code_type = 3
+        code = guard.generate_one_time_code(self.shared_secret)
+
+        update_data = {
+            'client_id': self.client_id,
+            'steamid': self.steamid,
+            'code_type': code_type,
+            'code': code
+        }
+
+        response = self.session.post(SteamUrl.UpdateAuthSessionWithSteamGuardCode_URL, data=update_data)        
+
+
+    def _pool_sessions_steam(self):
+
+        pool_data = {
+            'client_id': self.client_id,
+            'request_id': self.request_id
+        }
+
+        response = self.session.post(SteamUrl.PollAuthSessionStatus_URL, data=pool_data)
+        response_json = json.loads(response.text)
+        self.refresh_token = response_json["response"]["refresh_token"]
+
+    def _finallez_login(self):
+        sessionid = self.session.cookies["sessionid"]
+        redir = "https://steamcommunity.com/login/home/?goto="
+
+        finallez_data = {
+            'nonce': self.refresh_token,
+            'sessionid': sessionid,
+            'redir': redir
+        }
+
+        response = self.session.post("https://login.steampowered.com/jwt/finalizelogin", data=finallez_data)
+        return response
+
+
+    def _setstokens(self, fin_resp):
+        response_json = json.loads(fin_resp.text)
+        self.nonce_store = response_json["transfer_info"][0]["params"]["nonce"]
+        self.auth_store = response_json["transfer_info"][0]["params"]["auth"]
+        self.nonce_com = response_json["transfer_info"][1]["params"]["nonce"]
+        self.auth_com = response_json["transfer_info"][1]["params"]["auth"]
+
+        store_data = {
+            'nonce': self.nonce_store,
+            'auth': self.auth_store,
+            'steamID': self.steamid
+        }
+
+        com_data = {
+            'nonce': self.nonce_com,
+            'auth': self.auth_com,
+            'steamID': self.steamid
+        }
+
+        response = self.session.post(SteamUrl.Settoken_community_URL, data=com_data)
+        response = self.session.post(SteamUrl.Settoken_store_URL, data=store_data)
+        
